e860af8a09cc54f26181ae8a621c75f94b2ab1e2
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-09 22:28:59 -0300

    Completed the AlexNet class

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
-import tensorflow
-
 #Based on https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
+
+import tensorflow
+import numpy as np
+
 class AlexNet(object):
-    def __init__(self, x, keep_prob, num_classes, skip_layer):
+    def __init__(self, x, keep_prob, num_classes, skip_layer, pre_trained_path=None):
         self.X = x
         self.NUM_CLASSES = num_classes
         self.KEEP_PROB = keep_prob
         self.SKIP_LAYER = skip_layer
         self.IS_TRAINING = False
-        
+        self.WEIGHTS_PATH = pre_trained_path
 
     def create(self):
         pass
@@ -61,4 +63,69 @@ class AlexNet(object):
                 return act
     
     #Max pooling layer
-    
+    def max_pool(x, filter_height, filter_width, stride_y, stride_x,
+                 name, padding='SAME'):
+        return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
+                              strides = [1,stride_y, stride_x, 1],
+                              padding = padding, name = name)
+
+    #Batch normalization
+    def lrn(x, radius, alpha, beta, name, bias=1.0):
+        return tf.nn.loacL-response_normalization(x, depth_radius = radius,
+                                                  alpha = alpha, beta = beta,
+                                                  bias = bias, name = name)
+
+    #Dropout layer
+    def dropout(x, keep_prob):
+        return tf.nn.dropout(x, keep_prob)
+
+    def create(self):
+        # 1st Layer: Conv (w ReLu) -> Lrn -> Pool
+        conv1 = conv(self.X, 11, 11, 96, 4, 4, padding = 'VALID', name = 'conv1')
+        norm1 = lrn(conv1, 2, 2e-05, 0.75, name = 'norm1')
+        pool1 = max_pool(norm1, 3, 3, 2, 2, padding = 'VALID', name = 'pool1')
+
+        # 2nd Layer: Conv (w ReLu) -> Lrn -> Poolwith 2 groups
+        conv2 = conv(pool1, 5, 5, 256, 1, 1, groups = 2, name = 'conv2')
+        norm2 = lrn(conv2, 2, 2e-05, 0.75, name = 'norm2')
+        pool2 = max_pool(norm2, 3, 3, 2, 2, padding = 'VALID', name ='pool2')
+
+        # 3rd Layer: Conv (w ReLu)
+        conv3 = conv(pool2, 3, 3, 384, 1, 1, name = 'conv3')
+
+        # 4th Layer: Conv (w ReLu) splitted into two groups
+        conv4 = conv(conv3, 3, 3, 384, 1, 1, groups = 2, name = 'conv4')
+
+        # 5th Layer: Conv (w ReLu) -> Pool splitted into two groups
+        conv5 = conv(conv4, 3, 3, 256, 1, 1, groups = 2, name = 'conv5')
+        pool5 = max_pool(conv5, 3, 3, 2, 2, padding = 'VALID', name = 'pool5')
+
+        # 6th Layer: Flatten -> FC (w ReLu) -> Dropout
+        flattened = tf.reshape(pool5, [-1, 6*6*256])
+        fc6 = fc(flattened, 6*6*256, 4096, name='fc6')
+        dropout6 = dropout(fc6, self.KEEP_PROB)
+
+        # 7th Layer: FC (w ReLu) -> Dropout
+        fc7 = fc(dropout6, 4096, 4096, name = 'fc7')
+        dropout7 = dropout(fc7, self.KEEP_PROB)
+
+        # 8th Layer: FC and return unscaled activations
+        # (for tf.nn.softmax_cross_entropy_with_logits)
+        self.fc8 = fc(dropout7, 4096, self.NUM_CLASSES, relu = False, name='fc8')
+        
+
+    def load_pre_trained_weights(self, session):
+        weights_dict = np.load(self.WEIGHTS_PATH, encoding = 'bytes').item()
+
+        for op_name in weights_dict:
+            if op_name not in self.SKIP_LAYER:
+                with tf.variable_scope(op_name, reuse=True):
+                    for data in weights_dict[op_name]:
+                        if len(data.shape) == 1:
+                            var = tf.get_variable('biases', trainable=False)
+                            session.run(var.assign(data))
+                        else:
+                            var = tf.get_variable('weights', trainable=False)
+                            session.run(var.assign(data))
+
+        
