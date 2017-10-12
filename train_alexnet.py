fda45fece08321c858f64b7bbec461dff410177d
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-12 18:29:27 -0300

    Modified train code to use 60 classes and fixed bugs

--- train_alexnet.py
+++ train_alexnet.py
@@ -4,24 +4,26 @@ import tensorflow as tf
 import numpy as np
 import pickle
 import os
+from datetime import datetime
 from models import AlexNet
 from batch_making import *
 
 learning_rate = 0.001
 num_epochs = 10
-batch_size = 128
+batch_size = 50
 
 dropout_rate = 0.5
-num_classes = 50
+num_classes = 60
 train_layers = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8']
 
 display_step = 1
 
-filewriter_path = '/tmp/train_alexnet/cifar100'
-checkpoint_path = '/tmp/finetune_alexnet/'
+filewriter_path = 'cifar100_history/'
+checkpoint_path = 'checkpoints/'
 
-IMAGE_SIZE = 32
+IMAGE_SIZE = 227
 
+if not os.path.isdir(filewriter_path): os.mkdir(filewriter_path)
 if not os.path.isdir(checkpoint_path): os.mkdir(checkpoint_path)
 
 x = tf.placeholder(tf.float32, [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3])
@@ -56,7 +58,7 @@ with tf.name_scope('accuracy'):
     correct_pred = tf.equal(tf.argmax(score, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-tf.summary.scallar('accuracy', accuracy)
+tf.summary.scalar('accuracy', accuracy)
 
 # Merge all summaries together
 merged_summary = tf.summary.merge_all()
@@ -68,8 +70,8 @@ writer = tf.summary.FileWriter(filewriter_path)
 saver = tf.train.Saver()
 
 # Initalize the data generator seperately for the training and validation set
-train_generator = get_batches(train_data)
-val_generator = get_batches(test_data)
+train_generator = get_batches(train_data, batch_size)
+val_generator = get_batches(test_data, batch_size)
 
 # Get the number of training/validation steps per epoch
 train_batches_per_epoch = np.floor(size_train / batch_size).astype(np.int16)
@@ -86,7 +88,7 @@ with tf.Session() as sess:
   writer.add_graph(sess.graph)
 
   # Load the pretrained weights into the non-trainable layer
-  model.load_initial_weights(sess)
+  #model.load_initial_weights(sess)
 
   print("{} Start training...".format(datetime.now()))
   print("{} Open Tensorboard at --logdir {}".format(datetime.now(),
@@ -95,51 +97,51 @@ with tf.Session() as sess:
   # Loop over number of epochs
   for epoch in range(num_epochs):
 
-        print("{} Epoch number: {}".format(datetime.now(), epoch+1))
+    print("{} Epoch number: {}".format(datetime.now(), epoch+1))
 
-        step = 1
+    step = 1
 
-        while step < train_batches_per_epoch:
+    while step < train_batches_per_epoch:
 
-            # Get a batch of images and labels
-            batch_xs, batch_ys = train_generator.next_batch(batch_size)
+        # Get a batch of images and labels
+        batch_xs, batch_ys = train_generator.__next__()
 
-            # And run the training op
-            sess.run(train_op, feed_dict={x: batch_xs,
+        # And run the training op
+        sess.run(train_op, feed_dict={x: batch_xs,
                                           y: batch_ys,
                                           keep_prob: dropout_rate})
 
-            # Generate summary with the current batch of data and write to file
-            if step%display_step == 0:
-                s = sess.run(merged_summary, feed_dict={x: batch_xs,
+        # Generate summary with the current batch of data and write to file
+        if step%display_step == 0:
+            s = sess.run(merged_summary, feed_dict={x: batch_xs,
                                                         y: batch_ys,
                                                         keep_prob: 1.})
-                writer.add_summary(s, epoch*train_batches_per_epoch + step)
+            writer.add_summary(s, epoch*train_batches_per_epoch + step)
 
-            step += 1
+        step += 1
 
-        # Validate the model on the entire validation set
-        print("{} Start validation".format(datetime.now()))
-        test_acc = 0.
-        test_count = 0
-        for _ in range(val_batches_per_epoch):
-            batch_tx, batch_ty = val_generator.next_batch(batch_size)
-            acc = sess.run(accuracy, feed_dict={x: batch_tx,
+    # Validate the model on the entire validation set
+    print("{} Start validation".format(datetime.now()))
+    test_acc = 0.
+    test_count = 0
+    for _ in range(val_batches_per_epoch):
+        batch_tx, batch_ty = val_generator.__next__()
+        acc = sess.run(accuracy, feed_dict={x: batch_tx,
                                                 y: batch_ty,
                                                 keep_prob: 1.})
-            test_acc += acc
-            test_count += 1
-        test_acc /= test_count
-        print("Validation Accuracy = {:.4f}".format(datetime.now(), test_acc))
+        test_acc += acc
+        test_count += 1
+    test_acc /= test_count
+    print("Validation Accuracy = {:.4f}".format(datetime.now(), test_acc))
 
-        # Reset the file pointer of the image data generator
-        val_generator.reset_pointer()
-        train_generator.reset_pointer()
+    # Reset the file pointer of the image data generator
+    val_generator.reset_pointer()
+    train_generator.reset_pointer()
 
-        print("{} Saving checkpoint of model...".format(datetime.now()))
+    print("{} Saving checkpoint of model...".format(datetime.now()))
 
-        #save checkpoint of the model
-        checkpoint_name = os.path.join(checkpoint_path, 'model_epoch'+str(epoch)+'.ckpt')
-        save_path = saver.save(sess, checkpoint_name)
+    #save checkpoint of the model
+    checkpoint_name = os.path.join(checkpoint_path, 'model_epoch'+str(epoch)+'.ckpt')
+    save_path = saver.save(sess, checkpoint_name)
 
-        print("{} Model checkpoint saved at {}".format(datetime.now(), checkpoint_name))
+    print("{} Model checkpoint saved at {}".format(datetime.now(), checkpoint_name))
