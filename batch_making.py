c3fb216ee03c2ebef24dc83839f2cf8ec1d29c12
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-12 18:28:28 -0300

    Modified batch_making to create batches of images with 227x227 size

--- batch_making.py
+++ batch_making.py
@@ -1,3 +1,4 @@
+from img_util import *
 from sklearn.preprocessing import LabelBinarizer
 import numpy as np
 import pickle
@@ -5,6 +6,10 @@ import math
 import random
 
 random.seed(0)
+np.random.seed(0)
+
+NUM_CHANNELS = 3
+IMAGE_SIZE = 227
 
 print('LOADING DATA')
 target_data = pickle.load(open('pickle_files/target_data.pickle', 'rb'))
@@ -18,12 +23,18 @@ test_data = target_data[size_train:]
 vectorizer = pickle.load(open('pickle_files/vectorizer.pickle', 'rb'))
 print('DATA LOADED')
 
+def adjust_data(image_array):
+    image_matrix = image_array_to_image_matrix(image_array)
+    big_image = resize_image_matrix(image_matrix, IMAGE_SIZE, IMAGE_SIZE)
+    return big_image
+
 def get_batches(data, size_batch):
     len_data = len(data)
     num_batches = math.ceil(len_data/size_batch)
     for i in range(num_batches):
         new_batch = data[i*size_batch:min(len_data, (i+1)*size_batch)]
-        Xs = [b[0] for b in new_batch]
+        Xs = [adjust_data(b[0]) for b in new_batch]
+        
         raw_Ys = [b[1] for b in new_batch]
         Ys = vectorizer.transform(raw_Ys)
         yield [Xs, Ys]
