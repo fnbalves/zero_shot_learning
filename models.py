a33fe1d4b2f3dba21f81049ceb6397c9bcf2cd17
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-12 18:29:06 -0300

    Modified models to print input shapes for each layer

--- models.py
+++ models.py
@@ -1,7 +1,77 @@
 #Based on https://kratzert.github.io/2017/02/24/finetuning-alexnet-with-tensorflow.html
 
-import tensorflow
+import tensorflow as tf
 import numpy as np
+
+
+#Convolution function that can be split in multiple GPUS
+def conv(x, filter_height, filter_width, num_filters, stride_y, stride_x, name,
+             padding='SAME', groups=1, verbose_shapes=False):
+
+    #Get number of input chennels
+    input_channels = int(x.get_shape()[-1])
+
+    if verbose_shapes:
+        print('INPUT_CHANNELS', input_channels)
+        print('X SHAPE conv', x.get_shape())
+
+    convolve = lambda i, k: tf.nn.conv2d(i, k, strides = [1, stride_y, stride_x, 1],
+                                          padding = padding)
+
+    with tf.variable_scope(name) as scope:
+        weights = tf.get_variable('weights', shape = [filter_height, filter_width, input_channels/groups, num_filters])
+        biases = tf.get_variable('biases',shape=[num_filters])
+
+        if groups == 1:
+            conv = convolve(x, weights)
+        else:
+            input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
+            weight_groups = tf.split(axis=3, num_or_size_splits=groups, value=weights)
+            output_groups = [convolve(i, k) for i, k in zip(input_groups, weight_groups)]
+
+            conv = tf.concat(axis=3, values=output_groups)
+
+        bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
+        relu = tf.nn.relu(bias, name = scope.name)
+
+        return relu
+
+#Full connected layer
+def fc(x, num_in, num_out, name, relu=True):
+    with tf.variable_scope(name) as scope:
+        weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
+        biases = tf.get_variable('biases', [num_out], trainable=True)
+
+        act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
+
+        if relu == True:
+            relu = tf.nn.relu(act)
+            return relu
+        else:
+            return act
+    
+#Max pooling layer
+def max_pool(x, filter_height, filter_width, stride_y, stride_x,
+                name, padding='SAME', verbose_shapes=False):
+    if verbose_shapes:
+        print('X SHAPE maxpool', x.get_shape())
+
+    return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
+                              strides = [1,stride_y, stride_x, 1],
+                              padding = padding, name = name)
+
+#Batch normalization
+def lrn(x, radius, alpha, beta, name, bias=1.0, verbose_shapes=False):
+    if verbose_shapes:
+        print('X SHAPE lrn', x.get_shape())
+
+    return tf.nn.local_response_normalization(x, depth_radius = radius,
+                                                  alpha = alpha, beta = beta,
+                                                  bias = bias, name = name)
+
+#Dropout layer
+def dropout(x, keep_prob):
+    return tf.nn.dropout(x, keep_prob)
 
 class AlexNet(object):
     def __init__(self, x, keep_prob, num_classes, skip_layer, pre_trained_path=None):
@@ -11,73 +81,10 @@ class AlexNet(object):
         self.SKIP_LAYER = skip_layer
         self.IS_TRAINING = False
         self.WEIGHTS_PATH = pre_trained_path
-
-    def create(self):
-        pass
-
+        self.create()
+        
     def load_initial_weights(self):
         pass
-
-    #Convolution function that can be split in multiple GPUS
-    def conv(x, filter_height, filter_width, num_filters, stride_y, stride_x, name,
-             padding='SAME', groups=1):
-
-        #Get number of input chennels
-        input_channels = int(x.get_shape()[-1])
-
-        convolve = lambda i, k: tf.conv2d(i, k, strides = [1, stride_y, stride_x, 1],
-                                          padding = padding)
-
-        with tf.variable_scope(name) as scope:
-            weights = tf.get_variable('weights',
-					shape = [filter_height, filter_width, 
-						input_channels/groups, num_filters])
-	        biases = tf.get_variable('biases', shape = [num_filters])
-
-            if gourps == 1:
-                conv = convolce(x, weights)
-            else:
-                input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
-                weight_groups = tf.split(axis=3, num_or_size_splits=groups, value=weights)
-                output_groups = [convolve(i, k) for i, k in zip(input_groups, weight_groups)]
-
-                conv = tf.concat(axis=3, values=output_groups)
-
-            bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().ad_list())
-            relu = tf.nn.relu(bias, name = scope.name)
-
-            return relu
-
-    #Full connected layer
-    def fc(x, num_in, num_out, name, relu=True):
-        with tf.variable_scope(name) as scope:
-            weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
-            biases = tf.get_variable('biases', [num_out], trainable=True)
-
-            act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
-
-            if relu == True:
-                relu = tf.nn.relu(act)
-                return relu
-            else:
-                return act
-    
-    #Max pooling layer
-    def max_pool(x, filter_height, filter_width, stride_y, stride_x,
-                 name, padding='SAME'):
-        return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
-                              strides = [1,stride_y, stride_x, 1],
-                              padding = padding, name = name)
-
-    #Batch normalization
-    def lrn(x, radius, alpha, beta, name, bias=1.0):
-        return tf.nn.loacL-response_normalization(x, depth_radius = radius,
-                                                  alpha = alpha, beta = beta,
-                                                  bias = bias, name = name)
-
-    #Dropout layer
-    def dropout(x, keep_prob):
-        return tf.nn.dropout(x, keep_prob)
 
     def create(self):
         # 1st Layer: Conv (w ReLu) -> Lrn -> Pool
