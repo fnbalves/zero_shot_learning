4ae3f77149704854359abf821d857a023106dcf7
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-14 08:12:19 -0300

    Now the image size on the batch can be passed by argument

--- batch_making.py
+++ batch_making.py
@@ -10,7 +10,6 @@ random.seed(0)
 np.random.seed(0)
 
 NUM_CHANNELS = 3
-IMAGE_SIZE = 24
 
 print('LOADING DATA')
 target_train_data = pickle.load(open('pickle_files/target_train_data.pickle', 'rb'))
@@ -19,18 +18,18 @@ target_test_data = pickle.load(open('pickle_files/target_test_data.pickle', 'rb'
 vectorizer = pickle.load(open('pickle_files/vectorizer.pickle', 'rb'))
 print('DATA LOADED')
 
-def adjust_data(image_array):
+def adjust_data(image_array, image_size):
     image_matrix = image_array_to_image_matrix(image_array)
-    big_image = resize_image_matrix(image_matrix, IMAGE_SIZE, IMAGE_SIZE)
+    big_image = resize_image_matrix(image_matrix, image_size, image_size)
     return big_image
 
-def get_batches(data, size_batch):
+def get_batches(data, size_batch, image_size):
     random.shuffle(target_train_data)
     len_data = len(data)
     num_batches = math.floor(len_data/size_batch)
     for i in range(num_batches):
         new_batch = data[i*size_batch:min(len_data, (i+1)*size_batch)]
-        Xs = [adjust_data(b[0]) for b in new_batch]
+        Xs = [adjust_data(b[0], image_size) for b in new_batch]
         
         raw_Ys = [b[1] for b in new_batch]
         Ys = vectorizer.transform(raw_Ys)
