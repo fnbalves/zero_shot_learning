d3ff83b752ee76b1ead091fa02c3d967baeabe16
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-15 16:03:46 -0200

    Modified batch_making to generate word2vec batches if desired

--- batch_making.py
+++ batch_making.py
@@ -6,6 +6,13 @@ import math
 import random
 import tensorflow as tf
 
+USE_GLOVE = True
+
+if USE_GLOVE:
+    from glove_interface import *
+else:
+    from db_interface import *
+
 random.seed(0)
 np.random.seed(0)
 
@@ -23,7 +30,14 @@ def adjust_data(image_array, image_size):
     big_image = resize_image_matrix(image_matrix, image_size, image_size)
     return big_image
 
-def get_batches(data, size_batch, image_size):
+def word2vec_batch(word_batch):
+    new_batch = []
+    for word in word_batch:
+        wv = find_word_vec(normalize_label(word))
+        new_batch.append(wv)
+    return new_batch
+        
+def get_batches(data, size_batch, image_size, word2vec=False):
     random.shuffle(target_train_data)
     len_data = len(data)
     num_batches = math.floor(len_data/size_batch)
@@ -32,5 +46,9 @@ def get_batches(data, size_batch, image_size):
         Xs = [adjust_data(b[0], image_size) for b in new_batch]
         
         raw_Ys = [b[1] for b in new_batch]
-        Ys = vectorizer.transform(raw_Ys)
+        if not word2vec:
+            Ys = vectorizer.transform(raw_Ys)
+        else:
+            Ys = word2vec_batch(raw_Ys)
+        
         yield [Xs, Ys]
