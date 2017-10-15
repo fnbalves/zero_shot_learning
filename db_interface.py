45f30bf9aa663b005e7fd0a93adc82da14f0a6a7
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-15 15:07:26 -0200

    Added code to normalize composite labels

--- db_interface.py
+++ db_interface.py
@@ -13,6 +13,24 @@ print('DICTIONARY OPENED')
 db = client['word2vec']
 collection = db['words']
 
+composite_words = {
+        'pine_tree' : 'pine',
+        'sweet_pepper' : 'pepper',
+        'maple_tree' : 'maple',
+        'aquarium_fish' : 'fish',
+        'willow_tree' : 'willow',
+        'pickup_truck' : 'pickup',
+        'palm_tree' : 'palm',
+        'lawn_mower' : 'mower',
+        'oak_tree' : 'oak'
+}
+
+def normalize_label(label):
+        if label in composite_words:
+                return composite_words[label]
+        else:
+                return label
+
 def find_word_vec(word):
 	try:
 		object_id = word2id_dict[word]
