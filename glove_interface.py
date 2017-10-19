10027cae63f1a3d72ad433f26f8e6c43a2368e5d
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-10-19 00:03:57 -0200

    Trying to normalize the glove vectors

--- glove_interface.py
+++ glove_interface.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import csv
+import numpy as np
 
 glove_data_file = 'glove.6B/glove.6B.100d.txt'
 
 print('Loading glove model')
 words = pd.read_table(glove_data_file, sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
 print('Loaded')
+
+norm_mean = 5.5293 #3.9767720734252867
 
 composite_words = {
         'pine_tree' : 'pine',
@@ -25,8 +28,20 @@ def normalize_label(label):
         else:
                 return label
 
+def find_norm_mean():
+        all_words = words.index.values
+        count = .0
+        norm_sum = .0
+        
+        for w in all_words:
+                new_norm = np.linalg.norm(words.loc[w].as_matrix())
+                norm_sum += new_norm
+                count += 1
+        norm_sum /= count
+        return norm_sum
+
 def find_word_vec(word):
     try:
-        return words.loc[word].as_matrix()
+        return words.loc[word].as_matrix()/norm_mean
     except:
         return None
