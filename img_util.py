e96541045b68c4e6675140cdda2dfa692a13f20e
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-12-31 14:58:18 -0200

    Reformated file and added docstrings

--- img_util.py
+++ img_util.py
@@ -4,19 +4,25 @@ import scipy.misc
 
 NUM_CHANNELS = 3
 
+
 def image_array_to_image_matrix(image_array):
-    image_size = int(np.sqrt(np.prod(np.shape(image_array))/NUM_CHANNELS))
-    
+    """Gets a image matrix from CIFAR-100 and turns it into a matrix suitable for CNN Networks"""
+    image_size = int(np.sqrt(np.prod(np.shape(image_array)) / NUM_CHANNELS))
+
     image_matrix = image_array.reshape(NUM_CHANNELS, image_size, image_size).transpose(1, 2, 0)
     return image_matrix
 
+
 def image_matrix_to_image_array(image_matrix):
+    """Gets a image matrix for use in CNNs and converts it back to a CIFAR-100 like matrix"""
     image_size = np.shape(image_matrix)[1]
-    
-    image_array = image_matrix.transpose(2, 0, 1).reshape(image_size*image_size*NUM_CHANNELS,)
+
+    image_array = image_matrix.transpose(2, 0, 1).reshape(image_size * image_size * NUM_CHANNELS, )
     return image_array
-    
+
+
 def visualize_image(image):
+    """Displays the image in a CIFAR-100 matrix"""
     num_dims = len(np.shape(image))
     if num_dims == 1:
         image_to_visualize = image_array_to_image_matrix(image)
@@ -24,11 +30,13 @@ def visualize_image(image):
         image_to_visualize = image
     else:
         raise ValueError("Image array should have one or three dimensions")
-    
+
     plt.figure()
     plt.imshow(image_to_visualize, interpolation='nearest')
     plt.show()
 
+
 def resize_image_matrix(image_matrix, new_x_size, new_y_size):
+    """Resize a image matrix"""
     new_image = scipy.misc.imresize(image_matrix, (new_x_size, new_y_size))
     return new_image
