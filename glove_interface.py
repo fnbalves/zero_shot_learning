7c16619e7c831af26103c993836c94c5d6b4a173
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-12-31 14:58:05 -0200

    Reformated file and added docstrings

--- glove_interface.py
+++ glove_interface.py
@@ -8,42 +8,48 @@ print('Loading glove model')
 words = pd.read_table(glove_data_file, sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
 print('Loaded')
 
-norm_mean = 5.5293 #3.9767720734252867
+norm_mean = 5.5293
 
 composite_words = {
-        'pine_tree' : 'pine',
-        'sweet_pepper' : 'pepper',
-        'maple_tree' : 'maple',
-        'aquarium_fish' : 'fish',
-        'flatfish' : 'fish',
-        'willow_tree' : 'willow',
-        'pickup_truck' : 'pickup',
-        'palm_tree' : 'palm',
-        'lawn_mower' : 'mower',
-        'oak_tree' : 'oak',
-        'streetcar': 'car'
+    'pine_tree': 'pine',
+    'sweet_pepper': 'pepper',
+    'maple_tree': 'maple',
+    'aquarium_fish': 'fish',
+    'flatfish': 'fish',
+    'willow_tree': 'willow',
+    'pickup_truck': 'pickup',
+    'palm_tree': 'palm',
+    'lawn_mower': 'mower',
+    'oak_tree': 'oak',
+    'streetcar': 'car'
 }
 
+
 def normalize_label(label):
-        if label in composite_words:
-                return composite_words[label]
-        else:
-                return label
+    """Turn composite words to single words"""
+    if label in composite_words:
+        return composite_words[label]
+    else:
+        return label
+
 
 def find_norm_mean():
-        all_words = words.index.values
-        count = .0
-        norm_sum = .0
-        
-        for w in all_words:
-                new_norm = np.linalg.norm(words.loc[w].as_matrix())
-                norm_sum += new_norm
-                count += 1
-        norm_sum /= count
-        return norm_sum
+    """Find the mean norm of the word2vec representations"""
+    all_words = words.index.values
+    count = .0
+    norm_sum = .0
+
+    for w in all_words:
+        new_norm = np.linalg.norm(words.loc[w].as_matrix())
+        norm_sum += new_norm
+        count += 1
+    norm_sum /= count
+    return norm_sum
+
 
 def find_word_vec(word):
+    """Gets the word2vec representation from a word"""
     try:
-        return words.loc[word].as_matrix()/norm_mean
+        return words.loc[word].as_matrix() / norm_mean
     except:
         return None
