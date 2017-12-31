7c4641d45c5404a016bc1adf45f539cd5f08fa29
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-12-31 14:05:32 -0200

    Reformated code

--- batch_making.py
+++ batch_making.py
@@ -22,11 +22,13 @@ not_target_test_data = pickle.load(open('pickle_files/not_target_test_data.pickl
 vectorizer = pickle.load(open('pickle_files/vectorizer.pickle', 'rb'))
 print('DATA LOADED')
 
+
 def adjust_data(image_array, image_size):
     """Resize the image to the needs of the model"""
     image_matrix = image_array_to_image_matrix(image_array)
     resized_image = resize_image_matrix(image_matrix, image_size, image_size)
     return resized_image
+
 
 def word2vec_batch(word_batch):
     """Takes a word batch and convert it to a dense representation batch"""
@@ -35,16 +37,17 @@ def word2vec_batch(word_batch):
         wv = find_word_vec(normalize_label(word))
         new_batch.append(wv)
     return new_batch
-        
+
+
 def get_batches(data, size_batch, image_size, word2vec=False, send_raw_str=False):
     """Takes a batch of pairs (image, word2vec word) and creates data generators from it"""
     random.shuffle(target_train_data)
     len_data = len(data)
-    num_batches = math.floor(len_data/size_batch)
+    num_batches = math.floor(len_data / size_batch)
     for i in range(num_batches):
-        new_batch = data[i*size_batch:min(len_data, (i+1)*size_batch)]
+        new_batch = data[i * size_batch:min(len_data, (i + 1) * size_batch)]
         Xs = [adjust_data(b[0], image_size) for b in new_batch]
-        
+
         raw_Ys = [b[1] for b in new_batch]
         if not word2vec:
             Ys = vectorizer.transform(raw_Ys)
